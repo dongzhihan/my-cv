40907ee7c72f5ede424c31865da8e412d48df1e3
dongzhihan <529211933@qq.com>
2017-02-10 13:42:51 +0800

    update

--- cv/src/App.vue
+++ cv/src/App.vue
@@ -1,40 +1,61 @@
 <template>
-  <div id="app">  
-      <div> <span>{{name}}</span></div>
-     <div>
-            
-            <desco time="2000" company="cx">  </desco>
-     </div>
+  <div id="app">
+    <div class="name"> <span>{{name}}</span></div>
+
+    <div class="main">
+      <div>
+        <desco class="desco" time="2000" company="cx" experience="第一次经历"> </desco>
+        <desco class="desco" time="2000" company="cx" experience="第一次经历"> </desco>
+        <desco class="desco" time="2000" company="cx" experience="第一次经历"> </desco>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import desc from './components/desc.vue'
- 
-export default {
-    beforeCreate () {
+  import desc from './components/desc.vue'
+
+  export default {
+    beforeCreate() {
       console.log(desc)
     },
-   data () {
-     return {
-       name:"董智含"
-     }
-   },
-   
- 
-        components: {  
-            "desco": desc
-        }  
-}
-</script>
+    data() {
+      return {
+        name: "董智含"
+      }
+    },
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    components: {
+      "desco": desc
+    }
+  }
+
+</script>
+<style scoped>
+  #app {
+
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+        background: green
+  }
+  
+  .name {
+    text-align: center;
+    margin-bottom: 50px
+  }
+  
+  .desco {
+    width: 200px
+  }
+  
+  .main {
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 0;
+  }
+
 </style>
--- cv/src/components/desc.vue
+++ cv/src/components/desc.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="desc">
-    <h1> {{ time }} </h1>
-    <h1>{{company}}</h1>
-    <div class="te"></div>
+    <div class="time"><span>{{ time }}</span ><div><span class="company">{{company}}</span></div></div>
+    <div class="experience"><span class="experience-span">{{experience}}</span></div>
   </div>
 </template>
-
 <script>
   export default {
-    props: ["time","company"],
-    data: function() {
+    props: ["time", "company","experience"],
+    data: function () {
       return {
-        title: '' 
+        title: ''
       }
-    }
-    ,
-    
-  }
-</script>
+    },
 
-<style   lang="scss" scoped>
- $blue: #3bbfce;
-  .desc
-  {
-    background:$blue
-    
   }
-        
+
+</script>
+<style lang="scss" scoped>
+  $blue: #3bbfce;
+  .desc {
+    flex:1;
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 0;
+    background: $blue
+  }
+  
+  .company {
+    margin-left: 5px
+  }
+  
+ .time
+ {
+   width: 30%
+ }
+ .experience
+ {
+   width: 60%
+ }
+
 </style>
